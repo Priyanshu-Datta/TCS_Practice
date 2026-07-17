4e7604893d68067c894f07f54da41178441d4af0
Priyanshu-Datta <priyanshudatta80@gmail.com>
2026-07-18 01:14:01 +0530

    changes add 17

--- warehouse.java
+++ warehouse.java
@@ -9,6 +9,7 @@ public class warehouse {
 
         Arrays.sort(arr);
         int[][] pair = new int[n/2][2];
+        int check = 0;
 
         for(int i=0;i<n/2;i++){
             pair[i][0] = arr[start];
@@ -17,7 +18,14 @@ public class warehouse {
             start++;
             end--;
         }
+        for(int i=0;i<(n/2)-1;i++){
+            if(pair[i][0]+pair[i][1] != pair[i+1][0]+pair[i+1][1]){
+                check = -1;
+            }
+        }
         if(n % 2 != 0){
+            System.out.println(-1 + " Not possible");
+        }else if(check == -1){
             System.out.println(-1 + " Not possible");
         }else{
             for(int i=0;i<n/2;i++){
